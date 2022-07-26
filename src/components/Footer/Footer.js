4d7cfe819205339991a3eb22b7271f09759fd26e
thewebmasterp <thewebmasterp@gmail.com>
2022-07-26 21:27:27 +0300

    code built

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React from 'react'
 import './Footer.scss'
 import SocialMedias from '../SocialMedias/SocialMedias'
 
-const Footer = (props) => {
-    return (
-        <footer id="Footer">
-            <div id="copyright">
-                <p>Copyright © 2021 Zdravko Dimov, BG.  All rights reserved.</p>
-            </div>
-            <SocialMedias />
-        </footer>
-    )
+const Footer = props => {
+  return (
+    <footer id="Footer">
+      <div id="copyright">
+        <p>
+          Copyright 2020 Zdravko Dimov (<a href="https://thewebmasterp.com">
+            thewebmasterp
+          </a>). This project is{' '}
+          <a href="https://github.com/thewebmasterp/code-explainer">
+            open sourced
+          </a>.
+        </p>
+      </div>
+      <SocialMedias />
+    </footer>
+  )
 }
 
-export default Footer;
+export default Footer
